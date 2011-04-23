7fb800ba16203155ed262ab54d183d53fa35fbc1
Yanming Di <diy@stat.oregonstate.edu>
2011-04-23 00:00:00 +0000

    version 0.1.4

---
+++ R/nbp-classes.R
@@ -0,0 +1,152 @@
+## Functions for accessing elements in an "nbjObj"
+
+## Arguments:
+##
+##   obj: a list, output from nbp-mcle or nbp-test
+##
+
+get.mean.hat = function(obj, grp.id) {
+  r = length(obj$grp.ids);
+  grp = (1:r)[obj$grp.ids == grp.id][1];
+  mu = obj$pie[,grp] * obj$lib.sizes[grp];
+}
+
+get.var.hat = function(obj, grp.id) {
+  r = length(obj$grp.ids);
+  grp = (1:r)[obj$grp.ids == grp.id][1];
+  mu = obj$pie[,grp] * obj$lib.sizes[grp];
+  phi = obj$phi[grp];
+  v = mu + phi * mu^obj$alpha;
+}
+
+get.phi = function(obj, grp.id) {
+  r = length(obj$grp.ids);
+  grp = (1:r)[obj$grp.ids == grp.id][1];
+  phi = obj$phi[grp];
+}
+
+get.alpha = function(obj, grp.id) {
+  alpha = obj$alpha;
+}
+
+
+## Retrieve nbp parameters for one of the treatment groups from an nbp object
+get.nbp.pars = function(obj, grp.id) {
+## Arguments:
+##
+##   obj: output form nbp.mcle
+##
+##   grp.id: the id of a treatment grp
+##
+## Value:
+##
+##   n: number of genes
+##
+##   r: number of replicates
+##
+##   lib.sizes:
+##
+##   phi, alpha, pie:w
+##   
+  r = length(obj$grp.ids);
+  grp = (obj$grp.ids == grp.id);
+  idx = (1:r)[grp][1];
+
+  ## nbp.pars = new("nbpPars",
+  nbp.pars = list(
+       n = dim(obj$pseudo.counts)[1],
+       r = sum(grp),
+       grp.ids = obj$grp.ids[grp],
+       lib.sizes = obj$lib.sizes[idx],
+       pie = obj$pie[,idx],
+       phi = obj$phi[idx],
+       alpha = obj$alpha);
+}
+
+## Print nbp object
+print.nbp.counts = function(obj, subset=1:10) {
+## Arguments:
+##
+##   obj: an nbp obj
+##
+##   subset: 
+##
+## Value:
+##
+##   No return value.
+  n = dim(obj$counts)[1];
+  r = dim(obj$counts)[2];
+  cat(sprintf("Number of rows: %d\n", n));
+  cat(sprintf("Number of columns: %d\n", r));
+
+  cat("Groups:", sprintf("%s", obj$grp.ids), "\n");
+
+  cat("Counts:\n");
+  print(obj$counts[subset,]);
+  cat("...\n");
+  cat("Library sizes:", obj$lib.sizes, "\n");
+  cat("\n");
+  
+  cat("Pseudo counts:\n");
+  print(obj$pseudo.counts[subset,]);
+  cat("Pseudo library sizes:", obj$pseudo.lib.sizes, "\n");
+  cat("\n");
+
+  invisible();
+
+}
+
+## Print nbp.pars
+print.nbp.pars = function(obj, subset=1:10) {
+## Arguments:
+##
+##   obj: output form nbp.mcle
+##
+##   grp.id: the id of a treatment grp
+##
+## Value:
+##
+##   n: number of genes
+##
+##   r: number of replicates
+##
+##   lib.sizes:
+##
+##   phi, alpha, pie:w
+
+  cat(sprintf("Number of rows: %d\n", obj$n));
+  cat(sprintf("Number of columns: %d\n", obj$r));
+  cat("Groups:", obj$grp.ids, "\n");
+  cat("Library sizes:", obj$lib.sizes, "\n");
+  cat("Adjusted library sizes:", obj$pseudo.lib.sizes, "\n");
+  cat("Dispersion parameters:\n");
+  cat("    phi:", obj$phi,"\n");
+  cat("  alpha:", obj$alpha, "\n");
+  
+  cat("Mean relative frequencies:\n");
+  print(obj$pie[subset,], digits=8);
+  cat("...\n");
+  cat("\n");
+  
+  invisible();
+}
+
+
+## Print NBP test results
+print.nbp.test = function(obj, subset=1:10) {
+  cat("Expression levels and measures of differential expression:\n");
+  print(cbind(obj$expression.levels[subset,],
+              log.fc = obj$log.fc[subset],
+              p.value = obj$p.values[subset],
+              q.value = obj$q.values[subset]));
+  cat("\n");
+
+  invisible();
+}
+
+print.nbp = function(x, subset=1:10, ...) {
+  obj=x;
+  print.nbp.counts(obj, subset);
+  print.nbp.pars(obj, subset);
+  print.nbp.test(obj, subset);
+}
---
+++ R/nbp-main.R
@@ -0,0 +1,189 @@
+## Thin the counts
+thin.counts = function(y, current.lib.sizes = colSums(y),
+  target.lib.sizes = min(current.lib.sizes)) {
+  ## Arguments:
+  ##
+  ##   y: a n x r matrix of counts
+  ##
+  ##   current.lib.sizes: an r vector of currrent library sizes
+  ##
+  ##   norm.factors: an r vector of normalization factors
+  ##
+  ##   target.lib.sizes: target library sizes 
+
+  n = dim(y)[1];
+  ncols = dim(y)[2];
+
+  if (any (target.lib.sizes > current.lib.sizes)) {
+    stop("Error: taget.lib.sizes > current.lib.sizes!");
+  }
+
+  if (length(target.lib.sizes) == 1) {
+    target.lib.sizes = rep(target.lib.sizes, ncols);
+  }
+
+  y.thinned = y;
+
+  for (k in 1:ncols) {
+    keep.p =  target.lib.sizes[k]/current.lib.sizes[k]; # proportion of reads to keep
+    y.thinned[,k] = rbinom(n, y[,k], keep.p);
+  }
+
+  ## target.lib.sizes = rep(mean(colSums(y.thinned)), ncols);
+
+  list(counts =y.thinned, lib.sizes = target.lib.sizes);
+}
+
+test.thin.counts = function() {
+  set.seed(1001);
+  counts = rpois(1000, rexp(1000));
+  dim(counts) = c(200,5);
+  lib.sizes = colSums(counts);
+
+  ## Thin the counts
+  obj = thin.counts(counts);
+  obj$lib.sizes;
+  colSums(obj$counts);
+
+  for (k in 1:5) {
+    print(cor(counts[,k]/lib.sizes[k], obj$counts[,k]/obj$lib.sizes[k]));
+  }
+  
+  ## Thin the counts
+  obj = thin.counts(counts, target.lib.size = 100);
+  obj$lib.sizes;
+  colSums(obj$counts);
+
+  for (k in 1:5) {
+    print(cor(counts[,k]/lib.sizes[k], obj$counts[,k]/obj$lib.sizes[k]));
+  }
+
+  mean(obj$counts / counts, na.rm=TRUE);
+  mean(100 / lib.sizes);
+ 
+  ## checkEquals(y0,y1);
+}
+
+## Create an NBP object, perform coutn normalization, and adjust for
+## unequal library sizes.
+prepare.nbp = function(counts, grp.ids, lib.sizes = colSums(counts),
+  norm.factors = NULL,
+  thinning = TRUE,
+  print.level=1) {
+
+  ## Create NBP object
+  if (print.level > 0) cat("Create NBP data structure.\n");
+  obj = list(counts = counts, lib.sizes = lib.sizes, grp.ids = grp.ids);
+
+  ## Normalization
+
+  ## Currently, no normalization is implemented, but user-specified
+  ## normlization factors are supported.
+
+  if (is.null(norm.factors)) {
+    if (print.level>0) cat("No normalization is performed.\n");
+    norm.factors = rep(1, length(lib.sizes));
+  } else if (is.numeric(norm.factors)) {
+    if (print.level>0) cat("Use specified normalization factors.\n");
+    obj$norm.factors = norm.factors;
+  }
+
+  if (print.level > 1) {
+    cat("  Normalization factors:"); 
+    cat(norm.factors, sep=","); 
+    cat("\n");
+  }
+  obj$eff.lib.sizes = lib.sizes * norm.factors;
+    
+  ## Library size adjustment
+  if (thinning) {
+    if (print.level > 0) cat("Thin the counts to make library sizes approximately equal.\n");
+    res = thin.counts(counts, obj$eff.lib.sizes);
+    obj$pseudo.counts = res$counts;
+    obj$pseudo.lib.sizes = res$lib.sizes;
+  } else {
+    obj$pseudo.counts = counts;
+    obj$pseudo.lib.sizes = rep(mean(obj$eff.lib.sizes), length(obj$eff.lib.sizes));
+  }
+
+  obj
+}
+
+## Estimating NBP dispersion parameters
+estimate.disp = function(obj, method = "NBP", print.level=1, ...) {
+  ## Estimate the dispersion parameters using counts from ALL
+  ## treatment groups. In this step, we do not assume that genes have
+  ## the same mean relative counts under under different treatments.
+
+  ## Arguments:
+  ##
+  ##   obj: output from <prepare.nbp.obj>.
+  ##
+  ##   method: "NBP" (default) or "NB2", model for count variance (dispersion).
+  ##
+  ##   print.level: controls messages printed.
+
+  if (method =="NBP") {
+    nbp.est = nbp.mcle(obj$pseudo.counts, obj$pseudo.lib.sizes, obj$grp.ids,
+      print.level=print.level, ...);
+  } else if (method=="NB2") {
+    nbp.est = nb2.mcle(obj$pseudo.counts, obj$pseudo.lib.sizes, obj$grp.ids,
+      print.level=print.level, ...);
+  } else {
+    stop("Unknown method for estimating the dispersion parameters! Try NBP or NB2.");
+  }
+
+  res = obj;
+  res$phi = nbp.est$phi;
+  res$alpha = nbp.est$alpha;
+  res$pie = nbp.est$pie;
+
+  res;
+}
+
+## Perform exact test based on the NBP model.
+nbp.test = function(counts, grp.ids, grp1, grp2,
+  norm.factors = NULL,
+##  thinning = TRUE,
+  method.disp = "NBP",
+  print.level = 1,
+  ...) {
+
+  ## Arguments
+  ##
+  ##   counts: an n x r matrix of counts with rows corresponding to
+  ##   genes (exons, etc) and columns corresponding to libraries
+  ##   (samples).
+  ##
+  ##   grp.ids: an r vector denoting the treatment groups. For each
+  ##   gene, the mean counts within each group are assumed to be the
+  ##   same.
+  ##
+  ##   grp1, grp2: the two groups to be compared
+  ##
+  ##   norm.factors: normalization factors
+  ##
+  ##   method.disp: NBP (default) or NB2, method for estimating the
+  ##   dispersion parameters
+  ## 
+  ##   thinning: whethere the counts should be thinned so that the
+  ##   library sizes (adjusted for normalization factors) are roughy
+  ##   equal
+  ##
+  ##   ...: optional parameters to be passed to estimate.disp
+
+  thinning = TRUE;
+
+  ## Create an NBP object
+  obj = prepare.nbp(counts, grp.ids, norm.factors = norm.factors, thinning=thinning,
+    print.level=print.level);
+
+  ## Estimating count variance
+  obj = estimate.disp(obj, method = method.disp, print.level=print.level, ...);
+
+  ## Testing for differential gene expression
+  obj = exact.nb.test(obj, grp1, grp2, print.level=print.level);
+
+  ## class(obj) = "nbp.obj";
+  obj
+}
